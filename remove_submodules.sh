7d1708ff414b2ae8e6bdf4b027b25eceb849535f
1vanK <1vanK@users.noreply.github.com>
2024-11-15 00:43:53 +0300

    Create remove_submodules.sh

---
+++ remove_submodules.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+repo_dir=$(dirname "$0")
+
+remove_submodule()
+{
+    git -C "$repo_dir" deinit -f $1
+    rm -rf "$repo_dir/.git/modules/$1"
+    git -C "$repo_dir" rm -f $1
+}
+
+remove_submodule external/dviglo2d
+remove_submodule external/dv_big_int

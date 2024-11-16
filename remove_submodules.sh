f46109cb6ab0e151d5591020835614490e7c5c38
1vanK <1vanK@users.noreply.github.com>
2024-11-16 08:23:42 +0300

    Update remove_submodules.sh

--- remove_submodules.sh
+++ remove_submodules.sh
@@ -5,7 +5,7 @@ repo_dir=$(dirname "$0")
 remove_submodule()
 {
     git -C "$repo_dir" submodule deinit -f $1
-    rm -rf "$repo_dir/.git/modules/$1"
+    rm -rfv "$repo_dir/.git/modules/$1"
     git -C "$repo_dir" rm -f $1
 }
 
